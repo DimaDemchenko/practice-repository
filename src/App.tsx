aa9da0cad922f604e456ccf02c80b22d01e392fb
DimaDemchenko <ipz215_ddo@student.ztu.edu.ua>
2024-01-11 13:46:21 +0200

    fixed layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import './App.css'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
@@ -24,9 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React</p>
     </>
   )
 }
