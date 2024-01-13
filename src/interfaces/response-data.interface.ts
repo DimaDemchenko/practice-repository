1ca50c402a6d6032c2f55473f06315a1cd4bf9db
DimaDemchenko <ipz215_ddo@student.ztu.edu.ua>
2024-01-13 13:16:21 +0200

    created types for api response

---
+++ src/interfaces/response-data.interface.ts
@@ -0,0 +1,25 @@
+export interface Response {
+  status: Status
+  data: Data[]
+}
+
+export interface Status {
+  timestamp: string
+  error_code: number
+  error_message: null | string
+  elapsed: number
+  credit_count: number
+  notice: null | string
+}
+
+export interface Data {
+  id: number
+  rank: number
+  name: string
+  symbol: string
+  slug: string
+  is_active: number
+  first_historical_data: string
+  last_historical_data: string
+  platform: null | string
+}
