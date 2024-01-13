56aaac3bbae4870ef4c1e558db22b035ae8e6ad2
DimaDemchenko <ipz215_ddo@student.ztu.edu.ua>
2024-01-13 20:36:46 +0200

    implemented infiniteScroll component

--- src/components/infinite-scroll/InfiniteScroll.tsx
+++ src/components/infinite-scroll/InfiniteScroll.tsx
@@ -1,7 +1,67 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-const InfiniteScroll = () => {
-  return <div></div>
+import React, { useEffect, useRef, useState } from 'react'
+
+type GetDataFuncType<T> = (page: number) => Promise<T[]>
+
+type InfiniteScrollProps<T> = {
+  getDataFunc: GetDataFuncType<T>
+  renderItem: (item: T) => JSX.Element
+}
+
+const InfiniteScroll = <T,>({
+  getDataFunc,
+  renderItem,
+}: InfiniteScrollProps<T>) => {
+  const [items, setItems] = useState<T[]>([])
+  const [page, setPage] = useState<number>(1)
+
+  const fetchData = async () => {
+    const data = await getDataFunc(page)
+
+    if (data) {
+      setPage((prevPage) => prevPage + 1)
+      setItems((prevItems) => [...prevItems, ...data])
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const observerTarget = useRef(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          fetchData()
+        }
+      },
+      { threshold: 1 }
+    )
+
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current)
+    }
+
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current)
+      }
+    }
+  }, [observerTarget])
+
+  return (
+    <div>
+      <ul>
+        {items.map((item, index) => (
+          <React.Fragment key={index}>{renderItem(item)}</React.Fragment>
+        ))}
+      </ul>
+      <div ref={observerTarget}></div>
+    </div>
+  )
 }
 
 export default InfiniteScroll
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import InfiniteScroll from './components/infinite-scroll/InfiniteScroll'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <InfiniteScroll />
-  </React.StrictMode>
+  <React.StrictMode></React.StrictMode>
 )
