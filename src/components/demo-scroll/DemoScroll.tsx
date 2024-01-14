aaf376f2a7df0d259a4fbdfbfc93336d02ab62c3
DimaDemchenko <ipz215_ddo@student.ztu.edu.ua>
2024-01-14 20:39:55 +0200

    error handling

--- src/components/demo-scroll/DemoScroll.tsx
+++ src/components/demo-scroll/DemoScroll.tsx
@@ -3,19 +3,24 @@ import { Pokemon } from '../../interfaces/pokemon-data.interface'
 import InfiniteScroll from '../infinite-scroll/InfiniteScroll'
 import styles from './DemoScroll.module.css'
 
-const getDataFunc = async (page: number) => {
-  const baseUrl = import.meta.env.VITE_POKEMON_API_URL
+const getDataFunc = async (page: number): Promise<Pokemon[]> => {
+  try {
+    const baseUrl = import.meta.env.VITE_POKEMON_API_URL
 
-  const response = await axios.get(`${baseUrl}` + 'pokemon', {
-    params: {
-      limit: 10,
-      offset: page * 10,
-    },
-  })
+    const response = await axios.get(`${baseUrl}` + 'pokemon', {
+      params: {
+        limit: 10,
+        offset: page * 10,
+      },
+    })
 
-  const items: Pokemon[] = response.data.results
-  //console.log(response.data)
-  return items
+    const items: Pokemon[] = response.data.results
+    //console.log(response.data)
+    return items
+  } catch (error) {
+    console.log(error)
+    return []
+  }
 }
 
 const renderItem: (item: Pokemon) => JSX.Element = (item) => {
